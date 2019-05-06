21d5520170e19ab43c2c6b493dcdb2fcf1df3ac5
Willian Costa <willian.costa.a@gmail.com>
2019-05-06 20:42:24 -0300

    Devops teste

--- smartbank-env/teste-pratico.tf
+++ smartbank-env/teste-pratico.tf
@@ -1,5 +1,5 @@
-module "tesca_prd" {
-  source = "../cloud4erp-default/"
+module "teste-smartbank" {
+  source = "../smartbank-module/"
   name = "smartbank"
   class_instance = "t2.micro"
   size_volume = "200"
